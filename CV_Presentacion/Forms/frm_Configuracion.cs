f65756e8515e4a2987a239f165bcb5f5f699be6c
David0560 <137252234+David0560@users.noreply.github.com>
2024-06-06 17:29:17 -0300

    algoritmos de seguridad

--- CV_Presentacion/Forms/frm_Configuracion.cs
+++ CV_Presentacion/Forms/frm_Configuracion.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CL_Negocios.Entidades;
 
 namespace CV_Presentacion.Forms
 {
@@ -15,6 +16,18 @@ namespace CV_Presentacion.Forms
         public frm_Configuracion()
         {
             InitializeComponent();
+            
         }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Contraseña a = new Contraseña();
+            //textBox2.Text= a.crearContraseñaRandom(4,8,true,true,true,true);
+
+            label6.Text = Convert.ToString(a.crearCodigoVerificador(textBox1.Text, textBox2.Text));
+
+            label5.Text = a.crearSHA256(textBox1.Text, textBox2.Text);
+        }
+
     }
 }
