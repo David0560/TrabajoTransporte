41e806f6f74571532d986593019f13f936ddf7cd
Juanmyrock <juanmyrock@gmail.com>
2024-07-09 23:47:20 -0300

    v01

---
+++ CD_ConexionDatos/ConexionBD/cls_ConexionBD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD_ConexionDatos.ConexionBD
+{
+    public abstract class cls_ConexionBD
+    {
+
+    }
+}
