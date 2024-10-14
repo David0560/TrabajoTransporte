3257081d908fb8e472771035cd0b977eb8c8ab6a
Nicolas Sacco <sacconicolasf@gmail.com>
2024-10-14 15:50:29 -0300

    Mejorando la clase csProveedores

--- CapaServicios/Entidades/Registro/Proveedores.cs
+++ CapaServicios/Entidades/Registro/Proveedores.cs
@@ -9,21 +9,74 @@ namespace CapaServicios.Entidades
 {
     public class Proveedores
     {
-        public int Id { get; set; }
-        public string NombreEmpresa { get; set; }
-        public string Contacto { get; set; }
-        public int Telefono { get; set; }
-        public string Email { get; set; }
-        public string Ciudad { get; set; }
-        public string Pais { get; set; }
-        public string Calle { get; set; }
-        public string Numero { get; set; }
-        public int CP { get; set; }
+        // Atributos privados
+        private int id;
+        private string nombreEmpresa;
+        private string contacto;
+        private int telefono;
+        private string email;
+        private string ciudad;
+        private string pais;
+        private string calle;
+        private string numero;
+        private int cp;
 
-        public Proveedores(string nombreempresa, string contacto, int telefono, string email, 
+        // Propiedades públicas
+        public int Id
+        {
+            get { return id; }
+            private set { id = value; }
+        }
+        public string NombreEmpresa
+        {
+            get { return nombreEmpresa; }
+            set { nombreEmpresa = value; }
+        }
+        public string Contacto
+        {
+            get { return contacto; }
+            set { contacto = value; }
+        }
+        public int Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string Ciudad
+        {
+            get { return ciudad; }
+            set { ciudad = value; }
+        }
+        public string Pais
+        {
+            get { return pais; }
+            set { pais = value; }
+        }
+        public string Calle
+        {
+            get { return calle; }
+            set { calle = value; }
+        }
+        public string Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+        public int CP
+        {
+            get { return cp; }
+            set { cp = value; }
+        }
+
+        // Constructor
+        public Proveedores(string nombreempresa, string contacto, int telefono, string email,
             string ciudad, string pais, string calle, string numero, int cp)
         {
-            //No sé si pasar este o no, pero se genera en la DB   this.Id = id;
             this.NombreEmpresa = nombreempresa;
             this.Contacto = contacto;
             this.Telefono = telefono;
@@ -34,20 +87,15 @@ namespace CapaServicios.Entidades
             this.Numero = numero;
             this.CP = cp;
         }
-        public Proveedores(int id, string nombreempresa, string contacto, int telefono, string email, string ciudad, string pais, string calle, string numero, int cp)
+
+        public Proveedores(int id, string nombreempresa, string contacto, int telefono, string email,
+            string ciudad, string pais, string calle, string numero, int cp)
+            : this(nombreempresa, contacto, telefono, email, ciudad, pais, calle, numero, cp) // Llama al otro constructor
         {
             this.Id = id;
-            this.NombreEmpresa = nombreempresa;
-            this.Contacto = contacto;
-            this.Telefono = telefono;
-            this.Email = email;
-            this.Ciudad = ciudad;
-            this.Pais = pais;
-            this.Calle = calle;
-            this.Numero = numero;
-            this.CP = cp;
         }
     }
+
 
 }
 
